28c0f5dabb1c600801d165ed7ad192b807d95bc8
Alexander Davydov <davydov0@outlook.com>
2022-11-16 21:07:03 +0300

    11.16.21

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './normalize.css';
 import './app.scss';
+import styles from './app.module.scss';
 
-createRoot(document.getElementById('app')!).render(
-  <div
-    style={{
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      userSelect: 'none',
-      fontSize: '3rem',
-    }}
-  >
-    App
-  </div>
-);
+createRoot(document.getElementById('app')!).render(<div className={styles.app}>App</div>);
