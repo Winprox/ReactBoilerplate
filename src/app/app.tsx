278b795ea7a86505044580c014253c3104bb1699
Alexander Davydov <Winprox@icloud.com>
2022-12-28 01:49:35 +0300

    12.28.1

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,26 @@
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { createRoot } from 'react-dom/client';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+import create from '../utils/zustand';
 import './normalize.css';
 import './app.scss';
 import styles from './app.module.scss';
 
-createRoot(document.getElementById('app')!).render(<div className={styles.app}>App</div>);
+const store = create<{ count: number; increase: () => void }>((set) => ({
+  count: 0,
+  increase: () => set((s) => ({ count: s.count + 1 })),
+}));
+
+const App = () => {
+  const { count, increase } = store();
+  return (
+    <div className={styles.app} onClick={increase}>
+      {`Clicked ${count} times`}
+    </div>
+  );
+};
+
+if (process.env.NODE_ENV !== 'development') disableReactDevTools();
+else mountStoreDevtool('Store', store);
+
+createRoot(document.getElementById('app')!).render(<App />);
