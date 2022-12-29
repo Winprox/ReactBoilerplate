e92a3797c589782d9a3c804eb434fce52451b18d
Alexander Davydov <Winprox@icloud.com>
2022-12-29 22:13:28 +0300

    12.29.22

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-    chunkSizeWarningLimit: 2048,
-  },
+  build: { outDir: 'dist', assetsDir: '', chunkSizeWarningLimit: 2048 },
+  publicDir: './src/assets/public',
   plugins: [react()],
   server: { host: true },
-  publicDir: './src/assets/public',
 });
