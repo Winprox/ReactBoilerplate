32e612f7f335e17d87bb3ecff35506aea38a4e14
Alexander Davydov <Winprox@icloud.com>
2023-02-24 22:54:39 +0300

    2.24.22

--- build.sh
+++ build.sh
@@ -15,11 +15,11 @@ NEW_VER="$(date '+%-m').$(date '+%-d').$(date '+%-H')"
 sed -i "" -e "2s/.*/\"version\": \"${NEW_VER}\",/" package.json
 
 if ${REFRESH_MODULES}; then
-  rm -r node_modules
-  rm -r package-lock.json
-  rm -r yarn.lock
-  yarn i
+  rm -rf node_modules
+  rm -f package-lock.json
+  rm -f yarn.lock
+  yarn install
 fi
 
 rm -r dist
-yarn b
+yarn build
