fe7fe2b5ae2955fcd7c227707ce7cc89b88292ee
Alexander Davydov <Winprox@icloud.com>
2023-04-02 13:08:48 +0300

    4.2.13

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import paths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
-    assetsDir: './',
     minify: 'terser',
     terserOptions: { format: { comments: false } },
     chunkSizeWarningLimit: 2048,
