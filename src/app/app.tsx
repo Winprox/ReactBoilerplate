e1f934e7982273ea7b084a6438c655168fa522ea
Alexander Davydov <winprox@icloud.com>
2023-07-17 17:18:47 +0300

    7.17.17

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,40 +1,45 @@
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { createRoot } from 'react-dom/client';
 import { mountStoreDevtool } from 'simple-zustand-devtools';
-import { Consts } from '../utils/consts';
-import { create, proxy, TStore } from '../utils/zustand';
+import { cm, Consts, create, proxy, TStore } from '../utils';
 import './app.css';
 
 type TStoreState = { count: number; increase: () => void };
 const store: TStore<TStoreState> = create<TStoreState>((set) => ({
   count: 0,
-  increase: () => set((s) => ({ count: s.count + 1 })),
+  increase: () => set((s) => ({ count: s.count + 1 }))
 }));
 
 type TStore2State = { proxy: { count: number } };
 const store2: TStore<TStore2State> = create<TStore2State>(
-  () => ({
-    proxy: {
-      count: store.getState().count, //? default state
-    },
-  }),
+  () => ({ proxy: { count: store.getState().count } }), //? same default state
   [(s) => s.proxy.count, (c) => console.log(c)]
 );
-proxy(store, store2, [(s) => s.count, 'count']);
+
+proxy(store, store2, [(s) => s.count, (s) => s.proxy.count]);
 
 const App = () => {
   const { count, increase } = store();
   return (
     <div
-      className={
-        'root relative flex select-none flex-col items-center justify-center bg-white'
-      }
+      className={cm(
+        'root relative select-none bg-white',
+        'flex flex-col items-center justify-center'
+      )}
     >
       <button
-        className='rounded-md bg-blue-600 px-3 py-2 text-white shadow-md transition-all hover:scale-105 active:scale-95'
+        className={cm(
+          'rounded-md bg-blue-600 px-3 py-2 text-white shadow-md',
+          'transition-all hover:scale-105 active:scale-95'
+        )}
         onClick={increase}
       >{`Clicked ${count} times`}</button>
-      <div className='absolute left-0 top-0 m-5 flex w-auto flex-col gap-0 rounded-md bg-blue-600 px-3 py-2 leading-5 text-white shadow-md'>
+      <div
+        className={cm(
+          'absolute left-0 top-0 m-5 flex w-auto flex-col gap-0 px-3 py-2',
+          'rounded-md bg-blue-600 leading-5 text-white shadow-md'
+        )}
+      >
         <p>{`Version ${Consts.version}`}</p>
         <p>{`${Consts.isModeDev ? 'Development' : 'Production'} Build`}</p>
       </div>
---
+++ src/utils/utils.ts
@@ -0,0 +1,67 @@
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { StateCreator, StoreApi, UseBoundStore, create as zustandCreate } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
+
+export const cm = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+//? === Zustand ===
+
+export const create: TCreate = (config, ...listeners) => {
+  const store = zustandCreate(subscribeWithSelector(config));
+  for (const l of listeners) store.subscribe(l[0], l[1]);
+  return store;
+};
+
+export const proxy: TProxy = (from, to, ...listeners) => {
+  const keys = Object.keys(to.getState() as Record<string, any>);
+  for (const l of listeners) {
+    const path = l[1]
+      .toString()
+      .split('.')
+      .reverse()
+      .map((v) => v.replace(/[\n;} ]/g, ''));
+    const start = path.findLastIndex((v) => keys.includes(v)) + 1;
+    from.subscribe(l[0], (v) =>
+      to.setState(
+        (s) =>
+          ({
+            ...setObjValAtPath(s as Record<string, any>, path.slice(0, start), v)
+          }) as typeof s
+      )
+    );
+  }
+};
+
+const setObjValAtPath = (
+  obj: Record<string, any>,
+  path: string[],
+  val: any
+): Record<string, any> => {
+  const last = path.pop();
+  if (!last) return obj;
+  return Object.assign(obj, { [last]: path.length ? setObjValAtPath(obj, path, val) : val });
+};
+
+export type TStore<T> = UseBoundStore<Omit<StoreApi<T>, keyof TStoreSub<T>> & TStoreSub<T>>;
+type TStoreSub<T> = {
+  subscribe: {
+    (listener: (state: T, prevState: T) => void): () => void;
+    <U>(
+      selector: (state: T) => U,
+      listener: (state: U, prevState: U) => void,
+      options?: { equalityFn?: (a: U, b: U) => boolean; fireImmediately?: boolean }
+    ): () => void;
+  };
+};
+
+type TCreate = <T>(
+  config: StateCreator<T>,
+  ...listeners: [(state: T) => any, (state: any, prevState: any) => void][]
+) => TStore<T>;
+
+type TProxy = <T, U>(
+  from: TStore<T>,
+  to: TStore<U>,
+  ...listeners: [(fromState: T) => any, (toState: U) => any][]
+) => void;
